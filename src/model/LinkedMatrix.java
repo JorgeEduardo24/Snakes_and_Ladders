480dcde882bf294b2b78b74f91434d1d859de507
Jorge Eduardo Jojoa <jorgeeduardo2410@gmail.com>
2021-11-08 15:14:45 -0500

    createMatrix method added succesfully

--- src/model/LinkedMatrix.java
+++ src/model/LinkedMatrix.java
@@ -1,5 +1,43 @@
 package model;
 
 public class LinkedMatrix {
+	private Node first;
+	private int numberOfRows;
+	private int numberOfColumns;
 
+	public LinkedMatrix(int numberOfRows, int numberOfColumns) {
+		this.numberOfRows = numberOfRows;
+		this.numberOfColumns = numberOfColumns;
+		createMatrix();
+	}
+	
+	private void createMatrix() {
+		first = new Node(0,0);
+		createRow(0,0,first);
+	}
+	
+	private void createRow(int i, int j, Node currentFirstRow) {
+		createColumn(i,j+1,currentFirstRow,currentFirstRow.getUp());
+		if(i+1<numberOfRows) {
+			Node downFirstRow = new Node(i+1,j);
+			downFirstRow.setUp(currentFirstRow);
+			currentFirstRow.setDown(downFirstRow);
+			createRow(i+1,j,downFirstRow);
+		}
+	}
+	
+	private void createColumn(int i, int j, Node prev, Node rowPrev) { //Entra el previo para que el siguiente se pueda enlazar con el anterior
+		if(j<numberOfColumns) {
+			Node current = new Node(i,j);
+			current.setPrev(prev);
+			prev.setNext(current);
+			
+			if(rowPrev!=null) {
+				rowPrev = rowPrev.getNext();
+				current.setUp(rowPrev);
+				rowPrev.setDown(current);
+			}
+			createColumn(i,j+1,current,rowPrev.getNext()); //Current se vuelve el anterior del siguiente
+		}
+	}
 }
